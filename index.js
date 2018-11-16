9d1c683dc1c2b62dcb163d69b56240bb1ecb8bc7
David Lee <david@leedium.com>
2018-11-16 05:28:56 +0100

    snapshotting a working version

--- index.js
+++ index.js
@@ -22,27 +22,29 @@
  *                  --layoutName {name} - name of layoutto transfer
  **/
 
-const {spawn} = require('child_process');
+const { spawn } = require("child_process");
 const fs = require("fs-extra");
-const readline = require('readline');
-const argv = require('yargs').argv;
-const git = require('simple-git');
+const filehound = require("filehound");
+const readline = require("readline");
+const argv = require("yargs").argv;
+const git = require("simple-git");
 
-const config = require('./config');
+const config = require("./config");
 
-const GIT_PATH = '.';
+const GIT_PATH = ".";
 
-const WORKING_FOLDER = './working';
-const TEMP_FOLDER = './transfer';
+const WORKING_FOLDER = "./working";
+const TEMP_FOLDER = "./transfer";
 
 
-const BRANCH_MASTER = 'master';
-const BRANCH_SOURCE = 'source';
-const BRANCH_TARGET = 'target';
+const BRANCH_MASTER = "master";
+const BRANCH_SOURCE = "source";
+const BRANCH_TARGET = "target";
 
-const {dcu, plsu, all, layoutName, full, cleaner} = argv;
-const transferPaths = [];
+const { dcu, plsu, all, layoutName, full, cleaner } = argv;
 const pathsToBeRemoved = [];
+
+let transferPaths = [];
 
 /**
  * Grabs the target(Source Copied From) dcu source
@@ -51,23 +53,23 @@ const pathsToBeRemoved = [];
 function grabTarget() {
   return new Promise((resolve) => {
     process.chdir(WORKING_FOLDER);
-    console.log('GRABBING TARGET (currently deployed stable)', process.cwd());
-    const ls1 = spawn('dcu', ['--grab', '--clean', '--node', config.dcuServerTarget], {
+    console.log("GRABBING TARGET (currently deployed stable)", process.cwd());
+    const ls1 = spawn("dcu", ["--grab", "--clean", "--node", config.dcuServerTarget], {
       env: Object.assign({}, process.env, {
-        'CC_APPLICATION_KEY': config.apiKeyTarget
+        "CC_APPLICATION_KEY": config.apiKeyTarget
       })
     });
-    ls1.stdout.on('data', (chunk) => {
-      console.log(chunk.toString('utf-8'))
+    ls1.stdout.on("data", (chunk) => {
+      console.log(chunk.toString("utf-8"));
     });
-    ls1.on('close', () => {
-      console.log('...target branch download completed.');
-      process.chdir('../');
+    ls1.on("close", () => {
+      console.log("...target branch download completed.");
+      process.chdir("../");
       setTimeout(() => {
-        resolve()
+        resolve();
       }, config.taskDelay);
     });
-  })
+  });
 }
 
 /**
@@ -77,20 +79,20 @@ function grabTarget() {
 function grabSource() {
   return new Promise((resolve) => {
     process.chdir(WORKING_FOLDER);
-    console.log('GRABBING SOURCE (latest changes)', process.cwd());
-    const ls1 = spawn('dcu', ['--grab', '--clean', '--node', config.dcuServerSource], {
+    console.log("GRABBING SOURCE (latest changes)", process.cwd());
+    const ls1 = spawn("dcu", ["--grab", "--clean", "--node", config.dcuServerSource], {
       env: Object.assign({}, process.env, {
-        'CC_APPLICATION_KEY': config.apiKeySource
+        "CC_APPLICATION_KEY": config.apiKeySource
       })
     });
-    ls1.stdout.on('data', (chunk) => {
-      console.log(chunk.toString('utf-8'));
+    ls1.stdout.on("data", (chunk) => {
+      console.log(chunk.toString("utf-8"));
     });
-    ls1.on('close', () => {
-      console.log('...source branch download completed');
-      process.chdir('../');
+    ls1.on("close", () => {
+      console.log("...source branch download completed");
+      process.chdir("../");
       setTimeout(() => {
-        resolve()
+        resolve();
       }, config.taskDelay);
     });
   });
@@ -105,12 +107,12 @@ function grabSource() {
 function checkoutBranch(name, callback) {
   return new Promise((resolve) => {
     console.log(`checkoutBranch:${name}`);
-    git(GIT_PATH).raw(['checkout', name], () => {
+    git(GIT_PATH).raw(["checkout", name], () => {
       setTimeout(() => {
-        resolve()
+        resolve();
       }, config.taskDelay);
-    })
-  })
+    });
+  });
 }
 
 /**
@@ -122,12 +124,12 @@ function checkoutBranch(name, callback) {
 function mergeBranch(name, callback) {
   return new Promise((resolve) => {
     console.log(`mergeBranch:,${name} into target`);
-    git(GIT_PATH).raw(['merge', name, '-Xtheirs'], () => {
+    git(GIT_PATH).raw(["merge", name, "-Xtheirs"], () => {
       setTimeout(() => {
-        resolve()
+        resolve();
       }, config.taskDelay);
-    })
-  })
+    });
+  });
 }
 
 /**
@@ -136,13 +138,13 @@ function mergeBranch(name, callback) {
  */
 function addAll() {
   return new Promise((resolve) => {
-    console.log('addAll...');
-    git(GIT_PATH).raw(['add', '.'], () => {
+    console.log("addAll...");
+    git(GIT_PATH).raw(["add", "."], () => {
       setTimeout(() => {
-        resolve()
+        resolve();
       }, config.taskDelay);
-    })
-  })
+    });
+  });
 
 }
 
@@ -152,12 +154,12 @@ function addAll() {
  */
 function commit() {
   return new Promise((resolve) => {
-    console.log('commit...');
-    git(GIT_PATH).raw(['commit', '-m', 'committing latest changes'], () => {
+    console.log("commit...");
+    git(GIT_PATH).raw(["commit", "-m", "committing latest changes"], () => {
       setTimeout(() => {
-        resolve()
+        resolve();
       }, config.taskDelay);
-    })
+    });
   });
 }
 
@@ -170,12 +172,12 @@ function commit() {
 function deleteBranch(name) {
   return new Promise((resolve) => {
     console.log(`deleteLocalBranch:,${name}`);
-    git(GIT_PATH).raw(['branch', '-D', name], () => {
+    git(GIT_PATH).raw(["branch", "-D", name], () => {
       setTimeout(() => {
-        resolve()
+        resolve();
       }, config.taskDelay);
-    })
-  })
+    });
+  });
 }
 
 /**
@@ -187,12 +189,12 @@ function deleteBranch(name) {
 function createBranch(name) {
   return new Promise((resolve) => {
     console.log(`createBranch:${name}`);
-    git(GIT_PATH).raw(['checkout', '-B', name], () => {
+    git(GIT_PATH).raw(["checkout", "-B", name], () => {
       setTimeout(() => {
-        resolve()
+        resolve();
       }, config.taskDelay);
-    })
-  })
+    });
+  });
 }
 
 /**
@@ -203,21 +205,21 @@ function createBranch(name) {
 function getDiffs() {
 
   return new Promise((resolve) => {
-    console.log('getDiffs');
-    const ls1 = spawn('git', ['whatchanged', '-1', '--pretty=""'], {
+    console.log("getDiffs");
+    const ls1 = spawn("git", ["whatchanged", "-1", "--pretty=\"\""], {
       shell: true
     });
-    ls1.stdout.pipe(fs.createWriteStream('./whatchanged.txt'))
-    ls1.stdout.on('data', (chunk) => {
+    ls1.stdout.pipe(fs.createWriteStream("./whatchanged.txt"));
+    ls1.stdout.on("data", (chunk) => {
       // process.stdout.write(chunk);
     });
-    ls1.on('close', () => {
-      console.log('...created diff file...');
+    ls1.on("close", () => {
+      console.log("...created diff file...");
       setTimeout(() => {
-        resolve()
+        resolve();
       }, config.taskDelay);
     });
-  })
+  });
 }
 
 /**
@@ -227,55 +229,110 @@ function getDiffs() {
  */
 function processDiffs() {
   return new Promise((resolve) => {
-    const transferPathArrayTemp = [];
-    const deletePathArrayTemp = [];
-    let counter = 0;
+    const transferPathArrayTemp = {};
+
     const rl = readline.createInterface({
       input: fs.createReadStream(config.diffFilePath)
     });
-    rl.on('line', (line) => {
-      const infoArray = line.split('\t');
+    rl.on("line", (line) => {
+      const infoArray = line.split("\t");
       const pathString = infoArray[1];
-      const modType = infoArray[0].split(' ')[4];
-      const pathArray = pathString.split('/');
-
+      const modType = infoArray[0].split(" ")[4];
+      const pathArray = pathString.split("/");
 
       let path;
-      let pathDelete;
-      // if (pathArray.length === 2) {
-      // path = (`${pathArray[0]}/`);
-      path = pathArray.slice(0, pathArray.length-1).join('/');
-      // } else if (pathArray.length > 2) {
-      if (pathArray[1] === "Web Content" && !webContentFound) {
-        webContentFound = true;
-      }
-      // }
+      path = pathArray.slice(0, pathArray.length - 1).join("/");
+      if (modType !== "D" && modType.indexOf("R") < 0) {
 
+        console.log(modType, pathString);
 
-      if (modType !== "D" && modType.indexOf('R') < 0) {
         if (!transferPathArrayTemp[path]) {
-          counter += 1;
-          transferPathArrayTemp[`${path}`] = true;
-          if (path.indexOf('dcu-extensions-migrator') < 0) {
-            transferPaths.push(path);
+          if (path.indexOf("dcu-extensions-migrator") < 0) {
+            let sp = path.split("/");
+            // console.log(sp)
+            // const headPath = sp.slice(0, 2).join("/");
+            transferPaths.push({
+              path: path.split('/').slice(0, 3).join("/"),
+              extType: sp[1]
+            });
+            transferPathArrayTemp[`${path}`] = true;
           }
         }
-      } else {
-        // if (!deletePathArrayTemp[path]) {
-        //   pathsToBeRemoved.push(path);
-        //   deletePathArrayTemp[path] = true;
-        // }
       }
     });
-    rl.on('close', () => {
+
+    rl.on("close", () => {
+      const pathListSearched = {};
+      const filteredSearched = {};
+      console.log(transferPaths)
+      transferPaths = transferPaths.reduce((a, pathObj) => {
+        const {extType, path } = pathObj;
+        let extra = [];
+        if (!pathListSearched[pathObj.path]) {
+          pathListSearched[pathObj.path] = true;
+
+          // const directories = filehound.create()
+          //   .depth(4)
+          //   .paths(pathObj.path)
+          //   .directory().findSync()
+          //   .map(path => ({ type: "d", path }));
+
+          // const files = filehound.create()
+          //   .ignoreHiddenFiles()
+          //   .depth(0)
+          //   .paths(pathObj.path)
+          //   .findSync()
+          //   .map(path => ({ type: "f", path }));
+
+
+          switch (extType) {
+            case "element":
+            case "theme":
+              extra.push({extType,path});
+              break;
+          }
+          // a = a.concat(
+          // directories
+          // files
+          // );
+          a.push({
+            extType,
+            path
+          })
+        }
+        return a;
+      }, []).reduce(
+        (ac, pathObj) => {
+          let { extType, path } = pathObj;
+          const pSplit = path.split("/");
+          if (pSplit[3] === "instances" && pSplit.length > 4) {
+            path = pSplit.slice(0, 2).join("/");
+          }
+          else if (pSplit[3] === "instances" && pSplit.length === 4) {
+            // if (pSplit[3] === "instances" && pSplit.length === 4) {
+            return ac;
+            // }else{
+          }
+          // else if (pSplit[3] !== "instances") {
+          //   path = pSplit.slice(0, pSplit.length -1).join("/");
+          // }
+          if (!filteredSearched[path] )
+          {
+            filteredSearched[path] = true;
+            ac.push({ extType, path });
+          }
+          // ac.push(path);
+          return ac;
+        }, []);
+      console.log("\n\n\n", transferPaths);
       resolve();
     });
-  })
+  });
 }
 
 async function makeTmpFolder() {
-  return new Promise( async(resolve) => {
-    const workingTransfer = WORKING_FOLDER.split('/')[1];
+  return new Promise(async (resolve) => {
+    const workingTransfer = WORKING_FOLDER.split("/")[1];
     fs.ensureDirSync(`${TEMP_FOLDER}/${workingTransfer}/.ccc`);
     fs.copySync(`${WORKING_FOLDER}/.ccc/config.json`, `${TEMP_FOLDER}/${workingTransfer}/.ccc/config.json`);
     // deleteFilePath(pathsToBeRemoved.map(path => `${TEMP_FOLDER}/${workingTransfer}/${path}`));
@@ -283,18 +340,34 @@ async function makeTmpFolder() {
     // await deleteFilePath(pathsToBeRemoved.map(path => `${path}`));
     // await deleteFilePath(pathsToBeRemoved.map(path => `.ccc/${path}`));
     //
-    transferPaths.map((path) => {
-      const fa = path.split('/');
-      const f = fa.slice(0, fa.length).join('/');
+    transferPaths.map(({ type, path }) => {
+      const fa = path.split("/");
+      const f = fa.slice(0, fa.length).join("/");
       fa[1] = `.ccc/${fa[1]}`;
-      const c = fa.slice(0, fa.length).join('/');
-      fs.ensureDirSync(`${TEMP_FOLDER}/${f}`);
-      fs.ensureDirSync(`${TEMP_FOLDER}/${c}`);
+      let c = fa.slice(0, fa.length).join("/");
+
+      const isStack = path.lastIndexOf("stack") >= 0;
+
       try {
+        // if (type === "f") {
+        //   fs.ensureFileSync(`${TEMP_FOLDER}/${f}`);
+        //   fs.ensureFileSync(`${TEMP_FOLDER}/${c}.etag`);
+        //   fs.copySync(`${path}`, `${TEMP_FOLDER}/${f}`);
+        //   fs.copySync(`${c}.etag`, `${TEMP_FOLDER}/${c}.etag`);
+        // } else if (type === "d") {
+        fs.ensureDirSync(`${TEMP_FOLDER}/${f}`);
+        fs.ensureDirSync(`${TEMP_FOLDER}/${c}`);
         fs.copySync(`${path}`, `${TEMP_FOLDER}/${f}`);
-        fs.copySync(`${c}`, `${TEMP_FOLDER}/${c}`);
+        fs.copySync(c, `${TEMP_FOLDER}/${c}`);
+        // }
+
+        if (isStack) {
+          let file = `${c.split("/").slice(0, 4).join("/")}/stack.json`;
+          console.log(isStack, path, file);
+          fs.copySync(`${file}`, `${TEMP_FOLDER}/${file}`);
+        }
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
     });
     resolve();
@@ -307,14 +380,14 @@ async function makeTmpFolder() {
  */
 async function deleteFilePath(pathsToBeRemoved) {
   return new Promise(async (resolve) => {
-    console.log('Removing...', pathsToBeRemoved);
+    console.log("Removing...", pathsToBeRemoved);
     pathsToBeRemoved.map((item) => {
       fs.removeSync(item);
     });
     setTimeout(() => {
-      resolve()
+      resolve();
     }, config.taskDelay);
-  })
+  });
 }
 
 /**
@@ -322,26 +395,26 @@ async function deleteFilePath(pathsToBeRemoved) {
  * @returns {Promise<any>}
  */
 function transferAll() {
-  const workingTransfer = WORKING_FOLDER.split('/')[1];
-  // process.chdir(`${TEMP_FOLDER}/${workingTransfer}`);
+  const workingTransfer = WORKING_FOLDER.split("/")[1];
+  process.chdir(`${TEMP_FOLDER}/${workingTransfer}`);
   return new Promise((resolve) => {
     console.log(`Transferring all extensions start...`);
-    const ls1 = spawn(`dcu`, ['--transferAll', '.', '--node', config.dcuServerTarget, '-k', config.apiKeyTarget], {
+    const ls1 = spawn(`dcu`, ["--transferAll", ".", "--node", config.dcuServerTarget, "-k", config.apiKeyTarget], {
       env: Object.assign({}, process.env, {
-        'CC_APPLICATION_KEY': config.apiKeyTarget
+        "CC_APPLICATION_KEY": config.apiKeyTarget
       })
     });
-    ls1.stdout.on('data', (chunk) => {
-      console.log(chunk.toString('utf-8'));
+    ls1.stdout.on("data", (chunk) => {
+      console.log(chunk.toString("utf-8"));
     });
-    ls1.stderr.on('data', (chunk) => {
-      console.log('Error:', chunk.toString());
+    ls1.stderr.on("data", (chunk) => {
+      console.log("Error:", chunk.toString());
     });
-    ls1.on('close', () => {
+    ls1.on("close", () => {
       console.log(`... target updated`);
       resolve();
     });
-  })
+  });
 }
 
 /**
@@ -349,27 +422,27 @@ function transferAll() {
  */
 function plsuTransferSingle() {
 
-  if (typeof layoutName === 'undefined') {
-    throw new Error('--layoutName is not defined');
+  if (typeof layoutName === "undefined") {
+    throw new Error("--layoutName is not defined");
   }
   console.log(`Transfering layout:${layoutName} start...`);
 
-  const plsuSpawn = spawn('plsu', [
-    '--transfer',
-    '--node', config.dcuServerSource,
-    '--applicationKey', config.apiKeySource,
-    '--name', layoutName,
-    '--destinationNode', config.dcuServerTarget,
-    '--destinationApplicationKey', config.apiKeyTarget
+  const plsuSpawn = spawn("plsu", [
+    "--transfer",
+    "--node", config.dcuServerSource,
+    "--applicationKey", config.apiKeySource,
+    "--name", layoutName,
+    "--destinationNode", config.dcuServerTarget,
+    "--destinationApplicationKey", config.apiKeyTarget
   ]);
-  plsuSpawn.stdout.on('data', (chunk) => {
-    console.log(chunk.toString('utf-8'));
+  plsuSpawn.stdout.on("data", (chunk) => {
+    console.log(chunk.toString("utf-8"));
   });
-  plsuSpawn.stderr.on('data', (chunk) => {
+  plsuSpawn.stderr.on("data", (chunk) => {
     console.log(chunk);
   });
-  plsuSpawn.on('close', () => {
-    console.log('Transfer complete.');
+  plsuSpawn.on("close", () => {
+    console.log("Transfer complete.");
   });
 }
 
@@ -378,43 +451,43 @@ function plsuTransferSingle() {
  */
 async function plsuTransferAll() {
   return new Promise(resolve => {
-    console.log('TransferAll page layouts start...');
-    const plsuSpawn = spawn('plsu', [
-      '--transfer',
-      '--node', config.dcuServerSource,
-      '--applicationKey', config.apiKeySource,
-      '--all',
-      '--destinationNode', config.dcuServerTarget,
-      '--destinationApplicationKey', config.apiKeyTarget
+    console.log("TransferAll page layouts start...");
+    const plsuSpawn = spawn("plsu", [
+      "--transfer",
+      "--node", config.dcuServerSource,
+      "--applicationKey", config.apiKeySource,
+      "--all",
+      "--destinationNode", config.dcuServerTarget,
+      "--destinationApplicationKey", config.apiKeyTarget
     ]);
-    plsuSpawn.stdout.on('data', (chunk) => {
-      console.log(chunk.toString('utf-8'));
+    plsuSpawn.stdout.on("data", (chunk) => {
+      console.log(chunk.toString("utf-8"));
     });
-    plsuSpawn.stderr.on('data', (chunk) => {
-      console.log(chunk.toString('utf-8'));
+    plsuSpawn.stderr.on("data", (chunk) => {
+      console.log(chunk.toString("utf-8"));
     });
-    plsuSpawn.on('close', () => {
-      console.log('TransferAll page layouts complete.');
+    plsuSpawn.on("close", () => {
+      console.log("TransferAll page layouts complete.");
       setTimeout(() => {
-        resolve()
+        resolve();
       }, config.taskDelay);
     });
     resolve();
 
-  })
+  });
 }
 
 async function clean() {
   await deleteFilePath([
     TEMP_FOLDER,
     WORKING_FOLDER,
-    '.ccc',
-    'element',
-    'global',
-    'snippets',
-    'stack',
-    'theme',
-    'widget'
+    ".ccc",
+    "element",
+    "global",
+    "snippets",
+    "stack",
+    "theme",
+    "widget"
 
   ]);
   await checkoutBranch(BRANCH_MASTER);
@@ -432,22 +505,22 @@ async function clean() {
  */
 async function extensionsTransfer() {
   return new Promise(async (resolve) => {
-    await clean();
-    await grabTarget();
-    await addAll();
-    await commit();
-    await createBranch(BRANCH_TARGET);
-    await createBranch(BRANCH_SOURCE);
-    await grabSource();
-    await addAll();
-    await commit();
-    await checkoutBranch(BRANCH_TARGET);
-    await mergeBranch(BRANCH_SOURCE);
-    await getDiffs();
+    // await clean();
+    // await grabTarget();
+    // await addAll();
+    // await commit();
+    // await createBranch(BRANCH_TARGET);
+    // await createBranch(BRANCH_SOURCE);
+    // await grabSource();
+    // await addAll();
+    // await commit();
+    // await checkoutBranch(BRANCH_TARGET);
+    // await mergeBranch(BRANCH_SOURCE);
+    // await getDiffs();
     // await processDiffs();
     // await makeTmpFolder();
     // await deleteBranch(BRANCH_SOURCE);
-    // await transferAll();
+    //await transferAll();
     // await clean();
     resolve();
   });
@@ -458,7 +531,7 @@ async function extensionsTransfer() {
  * @returns {Promise<void>}
  */
 async function start() {
-  if(typeof cleaner !== 'undefined'){
+  if (typeof cleaner !== "undefined") {
     await clean();
     return;
   }
