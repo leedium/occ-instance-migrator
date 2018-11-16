835204938f5388237c8a8fb94a6c2e6de48ef98d
David Lee <david@leedium.com>
2018-11-16 07:20:06 +0100

    committing latest changes

--- index.js
+++ index.js
@@ -549,18 +549,18 @@ async function clean() {
  */
 async function extensionsTransfer() {
   return new Promise(async (resolve) => {
-    // await clean();
-    // await grabTarget();
-    // await addAll();
-    // await commit();
-    // await createBranch(BRANCH_TARGET);
-    // await createBranch(BRANCH_SOURCE);
-    // await grabSource();
-    // await addAll();
-    // await commit();
-    // await checkoutBranch(BRANCH_TARGET);
-    // await mergeBranch(BRANCH_SOURCE);
-    // await getDiffs();
+    await clean();
+    await grabTarget();
+    await addAll();
+    await commit();
+    await createBranch(BRANCH_TARGET);
+    await createBranch(BRANCH_SOURCE);
+    await grabSource();
+    await addAll();
+    await commit();
+    await checkoutBranch(BRANCH_TARGET);
+    await mergeBranch(BRANCH_SOURCE);
+    await getDiffs();
     // await processDiffs();
     // await makeTmpFolder();
     // await deleteFilePath([WORKING_FOLDER]);
